f347a2657fad226dd38811dc9af7b9f1cd829a5d
hjlee-kr-2025 <hjlee-kr@naver.com>
2025-01-23 15:05:11 +0900

    250123-5교시F.

--- my-app/src/ch05/Comment.jsx
+++ my-app/src/ch05/Comment.jsx
@@ -20,7 +20,7 @@ const styles = {
     height: 50,
     borderRadius: 25,
   },
-  contextContainer: {
+  contentContainer: {
     marginLeft: 8,
     display: 'flex',
     flexDirection: 'column',
@@ -40,8 +40,17 @@ const styles = {
 // 댓글하나를 표현(화면에 보여주기)하기 위한 컴포넌트
 function Comment(props) {
   return (
-    <div>
-      <h1>제가 만든 첫 컴포넌트 입니다.</h1>
+    <div style={styles.wrapper}>
+      <div style={styles.imageContainer}>
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png"
+          style={styles.image}
+        />
+      </div>
+      <div style={styles.contentContainer}>
+        <span style={styles.nameText}>이현진</span>
+        <span style={styles.commentText}>제가 만든 첫 컴포넌트입니다.</span>
+      </div>
     </div>
   );
 }
