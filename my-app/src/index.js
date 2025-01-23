3637f479f9260e4a6e7b5142bf200965350cdb73
hjlee-kr-2025 <hjlee-kr@naver.com>
2025-01-23 11:41:59 +0900

    250123-3교시F.-ch04F.

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,11 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Library from './ch03/Library';
+import Clock from './ch04/Clock';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+// setInterval(실행함수, 반복실행시간ms);
+setInterval(() =>
 root.render(
-    <Library />
-);
+    <Clock />
+), 1000);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
