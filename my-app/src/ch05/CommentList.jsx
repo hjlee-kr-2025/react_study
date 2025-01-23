24433679c915389d291b95f1fd4d88b5dcb31113
hjlee-kr-2025 <hjlee-kr@naver.com>
2025-01-23 15:11:43 +0900

    250123-ch05F.-댓글리스트컴포넌트.

--- my-app/src/ch05/CommentList.jsx
+++ my-app/src/ch05/CommentList.jsx
@@ -6,7 +6,9 @@ import Comment from "./Comment";
 function CommentList(props) {
   return (
     <div>
-      <Comment />
+      <Comment name={'이현진'} comment={'제가 만든 첫 컴포넌트 입니다.'}/>
+      <Comment name={'유재석'} comment={'런닝맨 사랑해주셔서 감사합니다.'}/>
+      <Comment name={'강민경'} comment={'리액트 재미있어요.'}/>
     </div>
   );
 }
