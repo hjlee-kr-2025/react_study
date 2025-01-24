196185c70929716289a781859af2399ff7a20599
hjlee-kr-2025 <hjlee-kr@naver.com>
2025-01-24 09:35:49 +0900

    250124-useState설명및구현.

--- my-app/src/ch07/Counter.jsx
+++ my-app/src/ch07/Counter.jsx
@@ -6,7 +6,11 @@
 import React, {useState} from "react";
 
 function Counter(props) {
-  var count = 0;//버튼 눌린 횟수를 저장할때 사용
+  // useState(초기값)를 실행하면 배열을 리턴합니다.
+  // 개발자가 정한 [변수이름, setter함수이름]으로 리턴받으면 됩니다.
+  // setter함수란? 객체안의 변수값을 변경할때 사용하는 함수입니다.
+  const [count, setCount] = useState(0);
+
   // react에서 화면에 변수값을 표시하려면 중괄호안에 변수이름을 적습니다. =>  {변수명}
   // react에서 이벤트 속성을 적용하려면 on다음글자를 대분자로 변경해서 적어줍니다. 예)  html - onclick => react - onClick (react규칙)
   // onClick={이벤트처리} 처럼 중괄호안에 이벤트처리리내용을 정의하셔야 합니다. ---> react규칙
@@ -14,7 +18,7 @@ function Counter(props) {
   return (
     <div>
       <p>총 {count}번 클릭했습니다.</p>
-      <button onClick={() => count++}>
+      <button onClick={() => setCount(count + 1)}>
         클릭
       </button>
     </div>
