eadaad8014da16f8311c1486ead769cebe04bb56
hjlee-kr-2025 <hjlee-kr@naver.com>
2025-01-31 15:07:25 +0900

    250131-props전달방식.

--- my-app14/src/App.js
+++ my-app14/src/App.js
@@ -1,24 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function Button(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <button>{props.theme}</button>
+  );
+}
+
+function ThemedButton(props) {
+  return (
+    <Button theme={props.theme} />
+  );
+}
+
+function Toolbar(props) {
+  return (
+    <div>
+      <ThemedButton theme={props.theme} />
     </div>
+  );
+}
+
+function App(props) {
+  return (
+    <Toolbar theme='dark' />
   );
 }
 
